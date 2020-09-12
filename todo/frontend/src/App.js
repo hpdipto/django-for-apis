60f1c13dd2c3415c1ad44e285c6530ef81486eff
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-12 11:51:51 +0600

    Frontend added

---
+++ todo/frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+	const [list, setList] = useState([]);
+
+	const getTodos = () => {
+		axios
+			.get("http://127.0.0.1:8000/api/")
+			.then((res) => {
+				setList(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	useEffect(() => {
+		getTodos();
+	}, []);
+
+	return (
+		<div>
+			{list.map((item) => (
+				<div key={item.id}>
+					<h1>{item.title}</h1>
+					<p>{item.body}</p>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default App;
