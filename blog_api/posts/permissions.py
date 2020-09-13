a4309778ba2bd3abc7f4c565df164c0f3c9838d8
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-13 07:19:46 +0600

    nothing changed

--- blog_api/posts/permissions.py
+++ blog_api/posts/permissions.py
@@ -5,7 +5,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 	def has_object_permission(self, request, view, obj):
 		# Read-only permissions are allowed for any request
 		if request.method in permissions.SAFE_METHODS:
-			print("##**")
 			return True
 
 		# Write permissions are only allowed to the author of a post
