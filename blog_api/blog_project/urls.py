42a7879b9539df0e2462b9f2273f523840d83ca1
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-14 07:31:12 +0600

    Docs added, but bug remains

--- blog_api/blog_project/urls.py
+++ blog_api/blog_project/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_schema_view(title="Blog API")
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A web API for creating and editing blog posts.'
+
+# schema_view = get_schema_view(title=API_TITLE)
+schema_view = get_swagger_view(title=API_TITLE)
+docs_view = include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +32,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('schema/', schema_view),
+    path('docs/', docs_view),
+    # path('schema/', schema_view),
+    path('swagger-docs/', schema_view),
 ]
